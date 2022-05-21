564bd5d12413cf1b0c4bfb0b5fcf9299c39c9b75
Boomslangnz <Boomslangnz@hotmail.com>
2022-05-21 16:12:09 +1200

    remove old crc and added Initial D 6 1.2v

--- dllmain.cpp
+++ dllmain.cpp
@@ -34,6 +34,7 @@ along with Output Blaster.If not, see < https://www.gnu.org/licenses/>.*/
 #include "Game Files/InitialD4.h"
 #include "Game Files/InitialD5.h"
 #include "Game Files/InitialD6.h"
+#include "Game Files/InitialD6Update.h"
 #include "Game Files/InitialD7.h"
 #include "Game Files/InitialD8.h"
 #include "Game Files/LGI.h"
@@ -62,183 +63,171 @@ Game* game;
 bool OutputsRunning = true;
 
 DWORD WINAPI OutputsLoop(LPVOID lpParam)
-{	
+{
 	Sleep(2500);
 
-	auto moduleBase = (uintptr_t)GetModuleHandle(nullptr);
-	if (*(uint32_t*)(moduleBase + 0x2182) == 0xE995C969)
+	// Craft CRC detection without virtual address
+	memcpy(newCrc, GetModuleHandle(nullptr), 0x400);
+	DWORD pePTR = *(DWORD*)(newCrc + 0x3C);
+
+	// Overwrite ImageBase with 8 bytes of 0
+	*(DWORD*)(newCrc + pePTR + 0x18) = 0x00000000;
+	*(DWORD*)(newCrc + pePTR + 0x18 + 4) = 0x00000000;
+#ifdef _AMD64_
+	* (DWORD*)(newCrc + pePTR + 50) = 0x00000000;
+#endif
+	* (DWORD*)(newCrc + pePTR + 54) = 0x00000000;
+	uint32_t newCrcResult = GetCRC32(newCrc, 0x400);
+	switch (newCrcResult)
 	{
+	case 0x74303cd6:
+		game = new OperationGhost;
+		break;
+	case 0xf26ecfa9:
+		game = new WackyRaces;
+		break;
+	case 0x1adfb24b:
 		game = new Machstorm;
-	}
-	if (*(uint32_t*)(moduleBase + 0x1140) == 0x2d7a80 || *(uint32_t*)(moduleBase + 0x10D0) == 0xC0850845 || *(uint32_t*)(moduleBase + 0x10D0) == 0xFF001F00)
-	{
+		break;
+	case 0x7787da54:
 		game = new MarioKartGPDXJP110;
+		break;
+	case 0x533f1a71:
+		game = new SonicAllStarsRacing;
+		break;
+	case 0x2c8b0265:
+		game = new LGI3D;
+		break;
+	case 0xd400a3f5:
+		game = new LGI;
+		break;
+	case 0x80900efd:
+		game = new VirtuaTennis4;
+		break;
+	case 0x92b5b16b:
+		game = new InitialD8;
+		break;
+	case 0x4fd57346:
+		game = new InitialD7;
+		break;
+	case 0x08d4bace:
+		game = new InitialD6Update; // v1.2
+		break;
+	case 0x715aaebf:
+		game = new InitialD6; // v1.0
+		break;
+	case 0x1b61779e:
+		game = new GTIClubSuperminiFesta;
+		break;
+	case 0x6844eee1:
+		game = new ChaseHQ2;
+		break;
+	case 0x47641574:
+		game = new SegaRacingClassic;
+		break;
+	case 0x5a468d9e:
+		game = new DaytonaChampionshipUSA;
+		break;
+	case 0xbafaca7b:
+		game = new BattleGear4Tuned;
+		break;
+	case 0x97994382:
+		game = new M2Emulator;
+		break;
+	case 0xE7BC4D6B:
+		game = new AliensExtermination;
+		break;
+	case 0xdc693790:
+	case 0x7dcef927:
+	case 0x8073dbb9:
+		game = new Transformers;
+		break;
+	case 0xbd8c984d:
+		game = new BattleGear4;
+		break;
+	case 0xed9b5740:
+		game = new H2Overdrive;
+		break;
+	case 0xfac8a714:
+		game = new Cars;
+		break;
+	case 0x8456EEC1:
+		game = new DirtyDrivin;
+		break;
+	case 0xc484002f:
+		game = new SRG;
+		break;
+	case 0xc68bcd2f:
+		game = new InitialD0V131;
+		break;
+	case 0x89da99ee:
+		game = new InitialD0V211;
+		break;
+	case 0xe75a6a44:
+		game = new WMMT5;
+		break;
+	case 0xDD61E0BA:
+		game = new WMMT5DX;
+		break;
+	case 0x1BB6F051:
+		game = new WMMT5DXPlus;
+		break;
+	case 0x0761cc11:
+		game = new WMMT6;
+		break;
+	case 0xa447f2ef:
+		game = new WMMT6R;
+		break;
+	default:
+#ifdef _DEBUG
+		static char test[256];
+		memset(test, 0, 256);
+		sprintf(test, "New CRC: %08x not implemented", newCrcResult);
+		OutputDebugStringA(test);
+#endif
+		break;
 	}
 
-	if (game != 0)
+	if (game != 0) //Load PC Based Arcade Game
 	{
 		game->OutputsGameLoop();
 		Sleep(16);
 	}
 	else
 	{
-		uint32_t crcResult = GetCRC32(GetModuleHandle(nullptr), 0x400);
-		switch (crcResult)
+		if (*(uint32_t*)0x804CA44 == 0x82EED98)
 		{
-		case 0xF8CEEC5D:
-			game = new SegaRacingClassic;
-			break;
-		case 0xb8f2836d:
-			game = new DaytonaChampionshipUSA;
-			break;
-		case 0x14eb3c8a:
-			game = new BattleGear4Tuned;
-			break;
-		case 0x1f2f9497:
-			game = new ChaseHQ2;
-			break;
-		case 0xf598cc36:
-			game = new OperationGhost;
-			break;
-		case 0x326ebcd6:
-			game = new GTIClubSuperminiFesta;
-			break;
-		case 0x379FA53E:
-			game = new InitialD6;
-			break;
-		case 0x1cd40f9e:
-			game = new InitialD7;
-			break;
-		case 0x080d35e3:
-			game = new InitialD8;
-			break;
-		case 0x82FED329:
-			game = new LGI;
-			break;
-		case 0x3f74055e:
-			game = new LGI3D;
-			break;
-		case 0xE7EAAEAD:
-			game = new SonicAllStarsRacing;
-			break;
-		case 0x6C6CD7D1:
-			game = new VirtuaTennis4;
-			break;
-		case 0x185fd019:
-			game = new WackyRaces;
-			break;
+			game = new AfterburnerClimax;
+		}
+		else if (*(uint32_t*)0x804B850 == 0x82642C8)
+		{
+			game = new Outrun2SP;
+		}
+		else if (*(uint32_t*)0x804A908 == 0x12EE)
+		{
+			game = new SRTV;
+		}
+		else if (*(uint32_t*)0x804CF84 == 0x1B09)
+		{
+			game = new InitialD4;
+		}
+		else if (*(uint32_t*)0x804D258 == 0x1C5F)
+		{
+			game = new InitialD5;
+		}
+		else if (*(uint32_t*)0x0804E8F8 == 0x08479718)
+		{
+			game = new RTuned;
 		}
 
-		// Craft CRC detection without virtual address
-		memcpy(newCrc, GetModuleHandle(nullptr), 0x400);
-		DWORD pePTR = *(DWORD*)(newCrc + 0x3C);
-
-		// Overwrite ImageBase with 8 bytes of 0
-		*(DWORD*)(newCrc + pePTR + 0x18) = 0x00000000;
-		*(DWORD*)(newCrc + pePTR + 0x18 + 4) = 0x00000000;
-#ifdef _AMD64_
-		* (DWORD*)(newCrc + pePTR + 50) = 0x00000000;
-#endif
-		* (DWORD*)(newCrc + pePTR + 54) = 0x00000000;
-		uint32_t newCrcResult = GetCRC32(newCrc, 0x400);
-		switch (newCrcResult)
-		{
-		case 0x97994382:
-			game = new M2Emulator;
-			break;
-		case 0xE7BC4D6B:
-			game = new AliensExtermination;
-			break;
-		case 0xdc693790:
-		case 0x7dcef927:
-			game = new Transformers;
-			break;
-		case 0xbd8c984d:
-			game = new BattleGear4;
-			break;
-		case 0xed9b5740:
-			game = new H2Overdrive;
-			break;
-		case 0xfac8a714:
-			game = new Cars;
-			break;
-		case 0x8456EEC1:
-			game = new DirtyDrivin;
-			break;
-		case 0xc484002f:
-			game = new SRG;
-			break;
-		case 0xc68bcd2f:
-			game = new InitialD0V131;
-			break;
-		case 0x89da99ee:
-			game = new InitialD0V211;
-			break;
-		case 0xe75a6a44:
-			game = new WMMT5;
-			break;
-		case 0xDD61E0BA:
-			game = new WMMT5DX;
-			break;
-		case 0x1BB6F051:
-			game = new WMMT5DXPlus;
-			break;
-		case 0x0761cc11:
-			game = new WMMT6;
-			break;
-		case 0xa447f2ef:
-			game = new WMMT6R;
-			break;
-		default:
-#ifdef _DEBUG
-			static char test[256];
-			memset(test, 0, 256);
-			sprintf(test, "New CRC: %08x not implemented", newCrcResult);
-			OutputDebugStringA(test);
-#endif
-			break;
-		}
-
-		if (game != 0) //Load PC Based Arcade Game
+		if (game != 0) //Load Lindbergh Game 
 		{
 			game->OutputsGameLoop();
 			Sleep(16);
 		}
 		else
 		{
-			if (*(uint32_t*)0x804CA44 == 0x82EED98)
-			{
-				game = new AfterburnerClimax;
-			}
-			//else if (*(uint32_t *)0x0832572E == 0xAAAA03C7)
-			//{
-			//	game = new HOTD4;
-			//}
-			else if (*(uint32_t*)0x804B850 == 0x82642C8)
-			{
-				game = new Outrun2SP;
-			}
-			else if (*(uint32_t*)0x804A908 == 0x12EE)
-			{
-				game = new SRTV;
-			}
-			else if (*(uint32_t*)0x804CF84 == 0x1B09)
-			{
-				game = new InitialD4;
-			}
-			else if (*(uint32_t*)0x804D258 == 0x1C5F)
-			{
-				game = new InitialD5;
-			}
-			else if (*(uint32_t*)0x0804E8F8 == 0x08479718)
-			{
-				game = new RTuned;
-			}
-			if (game != 0) //Load Lindbergh Game 
-			{
-				game->OutputsGameLoop();
-				Sleep(16);
-			}
+			MessageBoxA(NULL, "Output Blaster not supported!", "Error", NULL);
 		}
 	}
 	return 0;
