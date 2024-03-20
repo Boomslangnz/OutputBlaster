8459a5a372f7ac7190dc5c269ced1424cc5ed5ff
Mike Waye <mpzmail1@gmail.com>
2024-03-20 18:25:48 +0000

    Typo fix

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,7 +4,6 @@ Output Blaster is free software : you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-01a76797
 Output Blaster is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
