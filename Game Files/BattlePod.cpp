c33efaa0bed8c71bf6edecfe8146c55e28cbc553
mpzmail1 <mpzmail1@gmail.com>
2024-08-06 22:35:52 +0100

    Reworking BattlePod

--- Game Files/BattlePod.cpp
+++ Game Files/BattlePod.cpp
@@ -14,19 +14,63 @@ You should have received a copy of the GNU General Public License
 along with Output Blaster.If not, see < https://www.gnu.org/licenses/>.*/
 
 #include "BattlePod.h"
+#include <TlHelp32.h>
 
-uintptr_t gameAddress;
+DWORD GetProcessIDByName(const std::wstring& processName) {
+	DWORD processID = 0;
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshot != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32 pe;
+		pe.dwSize = sizeof(PROCESSENTRY32);
+		if (Process32First(hSnapshot, &pe)) {
+			do {
+				if (pe.szExeFile == processName) {
+					processID = pe.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnapshot, &pe));
+		}
+		CloseHandle(hSnapshot);
+	}
+	return processID;
+}
+static UINT64 baseAddress = 0x00007ff758ac0000;
+static HANDLE hProcess;
 
 static int WindowsLoop()
 {
-	int fan = helpers->ReadInt32(gameAddress + 0x1EE7A48, false);
-	int vibration = helpers->ReadInt32(gameAddress + 0x1EE7A3C, false);
-	int view = helpers->ReadInt32(gameAddress + 0x1EE7A64, false);
-	int ledRearPanelA = helpers->ReadInt32(gameAddress + 0x1EE7A58, false);
-	int ledRearPanelB = helpers->ReadInt32(gameAddress + 0x1EE7A5C, false);
-	int ledRearPanelC = helpers->ReadInt32(gameAddress + 0x1EE7A60, false);
-	int ledRearWindowsRGB = helpers->ReadInt32(gameAddress + 0x1EE7A54, false);
-	int ledRearCoverRGB = helpers->ReadInt32(gameAddress + 0x1EE7A50, false);
+	int air = 0;
+	int vibration = 0;
+	int view = 0;
+	int ledRearPanelA = 0;
+	int ledRearPanelB = 0;
+	int ledRearPanelC = 0;
+	int ledRearWindowsRGB = 0;
+	int ledRearCoverRGB = 0;
+
+	uintptr_t targetAddress = baseAddress + 0x1EE7A48;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &air, sizeof(air), NULL);
+
+	targetAddress = baseAddress + 0x1EE7A3C;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &vibration, sizeof(vibration), NULL);
+
+	targetAddress = baseAddress + 0x1EE7A64;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &view, sizeof(view), NULL);
+
+	targetAddress = baseAddress + 0x1EE7A58;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &ledRearPanelA, sizeof(ledRearPanelA), NULL);
+
+	targetAddress = baseAddress + 0x1EE7A5C;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &ledRearPanelB, sizeof(ledRearPanelB), NULL);
+
+	targetAddress = baseAddress + 0x1EE7A60;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &ledRearPanelC, sizeof(ledRearPanelC), NULL);
+
+	targetAddress = baseAddress + 0x1EE7A54;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &ledRearWindowsRGB, sizeof(ledRearWindowsRGB), NULL);
+
+	targetAddress = baseAddress + 0x1EE7A50;
+	ReadProcessMemory(hProcess, (LPCVOID)targetAddress, &ledRearCoverRGB, sizeof(ledRearCoverRGB), NULL);
 
 	int ledRearWindowsRed = (ledRearWindowsRGB >> 8) & 0xF;
 	int ledRearWindowsGreen = (ledRearWindowsRGB >> 4) & 0xF;
@@ -36,20 +80,18 @@ static int WindowsLoop()
 	int ledRearCoverGreen = (ledRearCoverRGB >> 4) & 0xF;
 	int ledRearCoverBlue = ledRearCoverRGB & 0xF;
 
-	Outputs->SetValue(Output1pAirFront, !!(fan != 0));
-	Outputs->SetValue(OutputLampView1, !!(view != 0));
-	Outputs->SetValue(OutputLampView2, !!(ledRearPanelA != 0));
-	Outputs->SetValue(OutputLampView3, !!(ledRearPanelB != 0));
-	Outputs->SetValue(OutputLampView4, !!(ledRearPanelC != 0));
+	Outputs->SetValue(Output1pAirFront, !!(air & 1));
+	Outputs->SetValue(OutputLampView1, !!(view & 1));
+	Outputs->SetValue(OutputLampView2, !!(ledRearPanelA & 1));
+	Outputs->SetValue(OutputLampView3, !!(ledRearPanelB & 1));
+	Outputs->SetValue(OutputLampView4, !!(ledRearPanelC & 1));
 	Outputs->SetValue(OutputLampRed, ledRearWindowsRed);
 	Outputs->SetValue(OutputLampGreen, ledRearWindowsGreen);
 	Outputs->SetValue(OutputLampBlue, ledRearWindowsBlue);
 	Outputs->SetValue(OutputLampRed2, ledRearCoverRed);
 	Outputs->SetValue(OutputLampGreen2, ledRearCoverGreen);
 	Outputs->SetValue(OutputLampBlue2, ledRearCoverBlue);
-
-	//vibration is converted to 0-100
-	Outputs->SetValue(OutputFFB, vibration / 16 * 100);
+	Outputs->SetValue(OutputFFB, (vibration / 16) * 100);
 	return 0;
 }
 
@@ -66,7 +108,15 @@ void BattlePod::OutputsGameLoop()
 {
 	if (!init)
 	{
-		gameAddress = (uintptr_t)GetModuleHandleA("SWArcGame-Win64-Shipping.exe");
+		std::wstring exeName = L"SWArcGame-Win64-Shipping.exe";
+		DWORD processID = GetProcessIDByName(exeName);
+		if (processID == 0) return;
+
+		hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, processID);
+		if (hProcess == NULL) {
+			return;
+		}
+
 		Outputs = CreateOutputsFromConfig();
 		m_game.name = "BattlePod";
 		Outputs->SetGame(m_game);
