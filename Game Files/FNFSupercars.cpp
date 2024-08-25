4fd09478656ded2fbddecdea6124379c72bafafd
mpzmail1 <mpzmail1@gmail.com>
2024-08-25 23:38:39 +0100

    Working FNF Outputs - Still need to do main menu lamps. Games fine tho!

--- Game Files/FNFSupercars.cpp
+++ Game Files/FNFSupercars.cpp
@@ -17,20 +17,19 @@ along with Output Blaster.If not, see < https://www.gnu.org/licenses/>.*/
 
 static int WindowsLoop()
 {
+	DWORD Start = helpers->ReadInt32( 0x731970, false);
+	DWORD View1 = helpers->ReadInt32( 0x731990, false);
+	DWORD View2 = helpers->ReadInt32( 0x7319B0, false);
+	DWORD View3 = helpers->ReadInt32( 0x7319D0, false);
+	DWORD Music = helpers->ReadInt32( 0x7319F0, false);
+	//DWORD RaceLeader = helpers->ReadByte( + 0x45271F4, false);
 
-	UINT8 Start = helpers->ReadByte(0x45271CC, true);
-	UINT8 View1 = helpers->ReadByte(0x45271D4, true);
-	UINT8 View2 = helpers->ReadByte(0x45271D8, true);
-	UINT8 View3 = helpers->ReadByte(0x45271DC, true);
-	UINT8 Music = helpers->ReadByte(0x45271E0, true);
-	UINT8 RaceLeader = helpers->ReadByte(0x45271F4, true);
-
-	Outputs->SetValue(OutputLampStart, !!(Start & 0xFF));
-	Outputs->SetValue(OutputLampView1, !!(View1 & 0xFF));
-	Outputs->SetValue(OutputLampView2, !!(View2 & 0xFF));
-	Outputs->SetValue(OutputLampView3, !!(View3 & 0xFF));
-	Outputs->SetValue(OutputLampView4, !!(Music & 0xFF));
-	Outputs->SetValue(OutputLampLeader, !!(RaceLeader & 0xFF));
+	Outputs->SetValue(OutputLampStart, !!(Start >= 1166012416));
+	Outputs->SetValue(OutputLampView1, !!(View1 >= 1166012416));
+	Outputs->SetValue(OutputLampView2, !!(View2 >= 1166012416));
+	Outputs->SetValue(OutputLampView3, !!(View3 >= 1166012416));
+	Outputs->SetValue(OutputLampView4, !!(Music >= 1166012416));
+	//Outputs->SetValue(OutputLampLeader, !!(RaceLeader & 0xFF));
 
 	return 0;
 }
@@ -44,6 +43,9 @@ static DWORD WINAPI OutputsAreGo(LPVOID lpParam)
 	}
 }
 
+
+
+
 void FNFSupercars::OutputsGameLoop()
 {
 	if (!init)
@@ -53,6 +55,7 @@ void FNFSupercars::OutputsGameLoop()
 		Outputs->SetGame(m_game);
 		Outputs->Initialize();
 		Outputs->Attached();
+		MH_EnableHook(MH_ALL_HOOKS);
 		CreateThread(NULL, 0, OutputsAreGo, NULL, 0, NULL);
 		while (GetMessage(&Msg1, NULL, NULL, 0))
 		{
